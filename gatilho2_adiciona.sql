4c2d91a7aa63192d3324c6d4be7031933c359012
AndreFCruz <andrecruz97@gmail.com>
2017-05-23 17:32:02 +0100

    New triggers

--- gatilho2_adiciona.sql
+++ gatilho2_adiciona.sql
@@ -1,14 +1,19 @@
 PRAGMA foreign_keys = on;
 
-DROP TRIGGER InsertOrderDish
+DROP TRIGGER InsertOrderDish;
 
 BEGIN TRANSACTION;
 
 CREATE TRIGGER InsertOrderDish
 Before insert on ROrder
 For each row
-When exists ()
+When exists (SELECT ROrder.Client FROM ROrder, Contains, Allergy WHERE 
+					Allergy.Client = ROrder.Client AND
+					Contains.Ingredient = Allergy.Ingredient AND
+					Contains.Dish = New.Dish AND
+					New.ROrder = ROrder.ID)
 BEGIN
-	
+	SELECT raise(rollback, 'Client is allergic to added dish');
+END;
 
 COMMIT TRANSACTION;
