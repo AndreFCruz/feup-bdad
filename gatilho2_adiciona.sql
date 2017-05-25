4ddc33516301f721f0cffec9b224e796acef9d98
AndreFCruz <andrecruz97@gmail.com>
2017-05-25 18:50:48 +0100

    fixed gatilho2 and povoar

--- gatilho2_adiciona.sql
+++ gatilho2_adiciona.sql
@@ -6,7 +6,8 @@ CREATE TRIGGER UpdateOrderTransaction
 after update on ROrder
 For each row
 When
-	(New.Time > (SELECT RTransaction.Time FROM RTransaction WHERE New.RTransaction = RTransaction.ID)) OR
+	(New.Time > (SELECT RTransaction.Time FROM RTransaction WHERE New.RTransaction = RTransaction.ID) AND
+		New.Date = (SELECT RTransaction.Date FROM RTransaction WHERE New.RTransaction = RTransaction.ID)) OR
 	(New.Date > (SELECT RTransaction.Date FROM RTransaction WHERE New.RTransaction = RTransaction.ID))
 BEGIN
 	SELECT raise(rollback, 'RTransaction must occur after ROrder');
