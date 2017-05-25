304d91a2d998248c831d30468d3f74e3c8afc064
AndreFCruz <andrecruz97@gmail.com>
2017-05-25 18:44:38 +0100

    column alignment

--- gatilho3_verifica.sql
+++ gatilho3_verifica.sql
@@ -1,11 +1,15 @@
+.mode	columns
+
+.headers	on
+
 --Test Trigger that verifies if the Client is allergic to the dish he is ordering.
 
 SELECT * FROM ROrder;
 SELECT * FROM RTransaction;
 SELECT * FROM OrderDish;
--- Error Test
+-- Test to Fail
 INSERT INTO OrderDish VALUES (23, 11);
 SELECT * FROM OrderDish;
--- Good Test
+-- Test to Pass
 INSERT INTO OrderDish VALUES (23, 10);
 SELECT * FROM OrderDish;
