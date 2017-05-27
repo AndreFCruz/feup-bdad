4626c59e7ba410f2dc2bdf54934266f1ed2c865a
AndreFCruz <andrecruz97@gmail.com>
2017-05-27 18:42:05 +0100

    Updated trigger 1

--- gatilho/gatilho1_adiciona.sql
+++ gatilho/gatilho1_adiciona.sql
@@ -1,14 +1,22 @@
 PRAGMA foreign_keys = on;
 
+-- Trigger that guarantees that a table booked for a reservation is not in use
+-- within a time interval of 1 hour.
+
 DROP TRIGGER if exists CheckTableAvailability;
 
 CREATE TRIGGER CheckTableAvailability
 before insert on ReservedTable
 For each row
 When exists
-	(SELECT ReservedTable.TableID FROM ReservedTable, Reservation WHERE
-		New.Reservation = Reservation.ID AND
-		Reservation.Time = )
+	(SELECT ReservedTable.TableID FROM ReservedTable, Reservation AS R1, Reservation AS R2
+	 WHERE
+		New.Reservation = R1.ID AND
+		R1.Time + 1 < R2.Time AND
+		R1.Date = R2.Date AND
+		R2.ID = ReservedTable.Reservation AND
+		ReservedTable.TableID = New.TableID AND
+		New.Restaurant = ReservedTable.Restaurant)
 BEGIN
 	SELECT raise(rollback, 'Reserved table was already booked');
 END;
--- gatilho/gatilho1_verifica.sql
+++ gatilho/gatilho1_verifica.sql
@@ -4,3 +4,18 @@
 
 -- Trigger that guarantees that a table booked for a reservation is not in use
 -- within a time interval of 1 hour.
+
+-- Overhead necessary for testing
+INSERT INTO Restaurant VALUES (-20, 102139520, "Test Restaurant 20", "Test Street 20");
+INSERT INTO Client VALUES (111000007, "Test Client", 111000007);
+DELETE FROM RTable WHERE ID = -2;
+INSERT INTO RTable VALUES (-5, -20);
+--
+
+INSERT INTO Reservation VALUES (-1, '19:00', 4, -20, 111000007, '2017-05-25');
+INSERT INTO Reservation VALUES (-2, '19:30', 4, -20, 111000007, '2017-05-25');
+INSERT INTO Reservation VALUES (-3, '20:05', 4, -20, 111000007, '2017-05-25');
+
+INSERT INTO ReservedTable VALUES (-1, -5, -20);
+INSERT INTO ReservedTable VALUES (-2, -5, -20);
+INSERT INTO ReservedTable VALUES (-3, -5, -20);
